3a617c0c509eaaa224bdd10e6f64aa847e299659
ErickWahyudy <erickwahyudy@gmail.com>
2022-12-02 22:26:59 +0700

    update banyak menu

--- speedtest.php
+++ speedtest.php
@@ -233,9 +233,8 @@
                     <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
                     <div class="position-relative mx-auto" style="max-width: 400px;">
                         <input class="form-control bg-transparent border-secondary w-100 py-3 ps-4 pe-5" type="text"
-                            placeholder="Your email">
-                        <button type="button"
-                            class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
+                            placeholder="any question ?" readonly>
+                        <a href="kassandra-wifi/contact.html" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">klik..</a>
                     </div>
                 </div>
             </div>
